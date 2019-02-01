9f3044fb1d80c78444a10d573bbb6fbdb4902822
APatrucco <47169304+APatrucco@users.noreply.github.com>
2019-02-01 11:06:45 -0500

    Update answers.js
    Added most of the answers to page 20.

--- answers.js
+++ answers.js
@@ -231,3 +231,131 @@ while (param2 <= param3)
     }
 }
 
+
+// Countdown
+
+function countdown(num);
+{
+    var newArr = [];
+    
+    for(num; num >= 0; num--)
+    {
+        newArr.push(num); //concatinates each new value of num to newArr
+    }
+    console.log(newArr.length); //prints the length of the new array
+    return newArr; //returns new array
+}
+
+
+// Print and Return
+
+function PrintReturn(arr) {
+    
+    console.log(arr[0]);  //prints first value of given two-value array
+    return arr[1];  //returns second value
+}
+
+
+// First Plus Length
+
+function firstPlusLength(arr) {
+    
+    var sum = ""; //create a variable to hold new sum
+    
+    sum = arr[0] + arr.length; //change value of sum to first value of given array + array length
+    return sum;
+}
+
+
+// Values Greater than Second
+
+var arr = [1,3,5,7,9,13]; //specified an array
+function valueGreater2(arr) {
+    
+    var newArr = []; //new array to hold values > 2nd value of given array
+    for(var x = 0; x < arr.length; x++) {
+        if(arr[x] > arr[1]) { //if value x of arr is > 2nd value
+            console.log(arr[x]); //print value x
+            newArr.push(arr[x]); //and add to new array
+        }
+    }
+    return newArr.length; //return length of new array
+}
+
+
+//Values Greater than Second, Generalized
+//Almost identical to previous function, but with error handling in the event that array length is too short
+
+function valueGreater2Gen(arr) {
+    
+    if(arr.length < 2) {
+        console.log("Array does not contain enough values.");
+        break;
+    }
+    else {
+        var newArr = []; //new array to hold values > 2nd value of given array
+        for(var x = 0; x < arr.length; x++) {
+            if(arr[x] > arr[1]) { //if value x of arr is > 2nd value
+                console.log(arr[x]); //print value x
+                newArr.push(arr[x]); //and add to new array
+            }
+        }
+        return newArr.length; //return length of new array
+    }
+}
+
+
+// This Length, That Value
+
+function lengthValue(num1, num2) {
+    
+    var newArr = []; //created a new empty array
+    newArr.length = num1; //gave it a length of num1
+    
+    for(var x = 0; x < newArr.length; x++) {
+        newArr[x] = num2; //changed each value of the new array to num2
+    }
+    if(num1 == num2) { //If num1 and num2 are the same
+        console.log("Jinx!"); //print "Jinx!"
+    }
+    return newArr; //returns new array
+}
+
+
+// Fit the First Value
+
+function fitFirst(arr) {
+    
+    if(arr[0] > arr.length) { //if first value of given array is greater than the length of the array
+        console.log("Too big!"); //it's too big
+    }
+    else if(arr[0] < arr.length) { //if first value of given array is less than the length of the array
+        console.log("Too small!"); //it's too small
+    }
+    else { //otherwise
+        console.log("Just right!"); //it's just right
+    }
+}
+
+
+// Farenheit to Celsius
+
+function farenheitToCelsius(fDegrees) { //function that accepts a value as Farenheit degrees
+    
+    var cDegrees = 0; //created a variable for Celsius
+    var degreeCalc = 0; //created a variable to hold the value of the conversion equation
+    
+    while(degreeCalc != fDegrees) { //for as long as the conversion equation is not equal to fDegrees
+        degreeCalc = (9/5 * cDegrees) + 32; //perform calculation
+        if(degreeCalc > fDegrees) { //if it's still greater than fDegrees
+            cDegrees - 0.1; //subtract 0.1
+        }
+        else if(degreeCalc < fDegrees) { //if less than fDegrees
+            cDegrees + 0.1; //add 0.1
+        }
+    }
+    return cDegrees; //when degreeCalc and fDegrees are equivalent, return cDegrees
+}
+
+
+// Celsius to Farenheit
